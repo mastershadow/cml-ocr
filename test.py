eb219212ddcc277b2ae39ffc46e6e2bcaeb55181
Eduard Roccatello <eduard.roccatello@3dgis.it>
2025-08-19 19:28:44 +0000

    dev

--- test.py
+++ test.py
@@ -4,22 +4,63 @@ import pytesseract
 image_path = "cene.png"
 im = cv2.imread(image_path)
 im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-(thresh, im_bw) = cv2.threshold(im_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-print(im_bw.shape)
-h = len(im_bw) - 1
-w = len(im_bw[0]) - 1
 
-im_temp = im_bw[0:h, 0:160]
-im_humid = im_bw[0:h, 200:350]
-im_dew = im_bw[0:h, 360:550]
-im_rain_tot = im_bw[0:h, 1100:1200]
-im_rain_cur = im_bw[0:h, w-260:w]
+(thresh, im_bw) = cv2.threshold(im_gray, 0, 255, cv2.THRESH_OTSU)
+h,w = im_bw.shape
 
-print(im_rain_cur.shape)
 
-cv2.imwrite("t.png", im_rain_tot)
+splits = []
+current_split = None
+for x in range(0, w):
+  has_content = False
+  for y in range(0, h):
+    if (im_bw[y, x] < 200):
+      has_content = True
+      break
+    
+  if has_content:
+    if current_split is None:
+      current_split = [x]
+  else:
+    if current_split is not None:
+      current_split.append(x - 1)
+      splits.append(current_split)
+      current_split = None
 
-custom_config = '--psm 13 --oem 3 -c tessedit_char_whitelist=0123456789'
+if current_split is not None:
+      current_split.append(x - 1)
+      splits.append(current_split)
+      current_split = None
+      
+# find white spaces
+thres = 20
+whites = []
+if splits[0][0] > thres:
+  whites.append([0, splits[0][0] - 1])
+  
+for idx in range(0, len(splits) - 1):
+  a = splits[idx]
+  b = splits[idx + 1]
+  if (b[0] - a[1] > thres):
+    whites.append([a[1] + 1, b[0] - 1])
+    
+if w - splits[len(splits) - 1][1] > thres:
+  whites.append([splits[len(splits) - 1][1] + 1, w])
+print(whites)
 
-extracted_text = pytesseract.image_to_string(im_rain_tot, lang="eng", config=custom_config)
-print(extracted_text)
+  
+
+
+im_temp = im_bw[:, 0:160]
+im_humid = im_bw[:, 200:350]
+im_dew = im_bw[:, 360:550]
+im_rain_tot = im_bw[:, 1100:1200]
+im_rain_cur = im_bw[:, w-260:w]
+
+custom_config = '--psm 7 --oem 3 -c tessedit_char_whitelist=0123456789'
+
+imgs = [im_temp, im_humid, im_dew, im_rain_tot, im_rain_cur]
+for idx, im in enumerate(imgs):
+  cv2.imwrite(str(idx) + ".png", im)
+  extracted_text = pytesseract.image_to_string(im, lang="eng", config=custom_config)
+  print(extracted_text)
